1580b3f1b731164f9a9548ecf0af22d8a6eea74d
anatolykorbasov <anatolykorbasov@gmail.com>
2023-03-20 00:06:25 +0300

    Learn JS

--- src/main/resources/templates/js/main.js
+++ src/main/resources/templates/js/main.js
@@ -1,17 +1,35 @@
-/**
- *
- * */
+// alert('lolkek')
 
-$(document).ready(function () {
 
-    $('.table .eBtn').on('click',function (event) {
-        event.preventDefault();
-        var href = $(this).attr('href');
+/*
 
-        $.get(href,function(){
+let test = 12
+const kek = 22
+const name = 'Oleg'
+const age = 20
+const output = 'My name is ' + name + ' and i am a ' + age + ' years old.'
+const output_good = `My name is ${name} and i am a ${age} years old.`
+console.log(output);
+console.log(output_good);
 
-        });
+function createMember(name) {
+    return function(lastName) {
+        console.log(name + lastName)
+    }
+}
 
-        $('.myForm #exampleModal').modal;
-    });
-});
+const logWithLastName =  createMember('Vladilen')
+console.log(logWithLastName)
+
+console.log(logWithLastName('Minin'))
+*/
+
+const cars = ['Lexus', 'BMW', 'Mercedes', 'Ford']
+
+console.log(cars)
+
+
+const text = 'Oh shit, here we go again.'
+
+const reverseText = text.split('').reverse().join('') // split - Делит строчку на символы и преображает в массив. Мы задаем по какому символу делить.  В нашем примере это ничего. .join() - преобразовывает массив в строку
+console.log(reverseText);
---
+++ src/main/resources/templates/main.js
@@ -0,0 +1,20 @@
+/**
+ *
+ * */
+
+/*
+
+$(document).ready(function () {
+
+    $('.table .eBtn').on('click',function (event) {
+        event.preventDefault();
+        var href = $(this).attr('href');
+
+        $.get(href,function(){
+
+        });
+
+        $('.myForm #exampleModal').modal;
+    });
+});
+*/
